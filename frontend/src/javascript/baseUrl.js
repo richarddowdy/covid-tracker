408825434dd06e27bf819c82ae36e6b26006cced
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-12 02:27:17 -0700

    oops

--- frontend/src/javascript/baseUrl.js
+++ frontend/src/javascript/baseUrl.js
@@ -1,7 +1,7 @@
-let BASE_URL;
+let BASE_API;
 
 process.env.NODE_ENV === "production"
   ? (BASE_API = `${window.location.origin}/api`)
   : (BASE_API = "http://localhost:3001/api/");
 
-export { BASE_URL };
+export { BASE_API };
