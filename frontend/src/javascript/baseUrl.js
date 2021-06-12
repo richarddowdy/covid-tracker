e449ce2e528caee98f4d298dffe48495e6bbc4ff
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-12 02:33:17 -0700

    third times the charm

--- frontend/src/javascript/baseUrl.js
+++ frontend/src/javascript/baseUrl.js
@@ -1,7 +1,7 @@
 let BASE_URL;
 
 process.env.NODE_ENV === "production"
-  ? (BASE_URL = `${window.location.origin}/api`)
+  ? (BASE_URL = `${window.location.origin}/api/`)
   : (BASE_URL = "http://localhost:3001/api/");
 
 export { BASE_URL };
